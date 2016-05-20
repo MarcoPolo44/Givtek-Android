45bdbc7f08664f12801cadcc53ac49994c082aa6
Marco Politakis <marco.politakis@gmail.com>
2016-05-20 22:05:08 +1200

    Added an image and button to the charity page.

--- app/src/main/java/com/MarcoPolitakisGmailCom/GivtekAndroid9Cl/DonateActivity.java
+++ app/src/main/java/com/MarcoPolitakisGmailCom/GivtekAndroid9Cl/DonateActivity.java
@@ -6,21 +6,32 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.net.URI;
 
 /**
  * Created by Marco on 5/19/2016.
  */
 public class DonateActivity extends Activity {
     TextView charityText;
+    ImageView charityImage;
+    Button donateButton;
+    Uri charityUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.donate_layout);
+        charityText = (TextView) findViewById(R.id.charityText);
+        charityImage =(ImageView) findViewById(R.id.charityImage);
+        donateButton = (Button) findViewById(R.id.donateButton);
 
-        charityText = (TextView) findViewById(R.id.charity_id);
         displayCharity();
     }
 
@@ -28,14 +39,26 @@ public class DonateActivity extends Activity {
         MyApplication context = ((MyApplication) getApplicationContext());
         context.getBeaconKey();
 
+        charityUri = null;
+
         if (context.getBeaconKey().equals("33211:2659"))
         {
             charityText.setText("Ronald McDonald House Charities®");
+            charityImage.setImageResource(R.drawable.rmhcnz);
+            charityUri = Uri.parse("http://nokket.com/");
 
             showNotification(
                     "Givtek - Donation Nearby",
                     context.getBeaconKey());
         }
+
+        donateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(Intent.ACTION_VIEW, charityUri);
+                startActivity(intent);
+            }
+        });
     }
 
     public void showNotification(String title, String message) {
